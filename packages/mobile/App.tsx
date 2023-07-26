ec06ae4f6b9f16c257a491492423cc3d576644d3
SugandSingh <sugandsigh5566@gmail.com>
2023-07-26 17:52:06 +0530

    added code for ios also

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
-import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./api";
-import ToDo from "./components/Todo";
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+} from 'react-native';
+import {addToDo, getAllToDo, updateToDo, deleteToDo} from './api';
+import ToDo from './components/Todo';
 
 const App: React.FC = () => {
   // State variables to manage ToDo items and their properties
-  const [toDo, setToDo] = useState<Array<{ _id: string; text: string }>>([]);
-  const [text, setText] = useState<string>("");
+  const [toDo, setToDo] = useState<Array<{_id: string; text: string}>>([]);
+  const [text, setText] = useState<string>('');
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
-  const [toDoId, setToDoId] = useState<string>("");
+  const [toDoId, setToDoId] = useState<string>('');
 
   // Fetch all ToDo items from the server on component mount
   useEffect(() => {
-    getAllToDo(setToDo);
+    getAllToDo(setToDo, Platform.OS);
   }, []);
 
   // Function to enter update mode for a specific ToDo item
@@ -26,46 +35,48 @@ const App: React.FC = () => {
   const handleAddOrUpdateToDo = () => {
     if (isUpdating) {
       // If in update mode, call updateToDo function
-      updateToDo(toDoId, text, setToDo, setText, setIsUpdating);
+      updateToDo(toDoId, text, setToDo, setText, setIsUpdating, Platform.OS);
     } else {
       // If not in update mode, call addToDo function
-      addToDo(text, setText, setToDo);
+      addToDo(text, setText, setToDo, Platform.OS);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ToDo App</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={{flex: 1,paddingHorizontal:12}}>
+        <Text style={styles.title}>ToDo App</Text>
 
-      <View style={styles.top}>
-        {/* Input field to add new ToDo items */}
-        <TextInput
-          style={styles.input}
-          placeholder="Add ToDos..."
-          value={text} 
-          onChangeText={(value) => setText(value)}
-        />
-
-        {/* Add or Update button based on the current mode */}
-        <TouchableOpacity style={styles.add} onPress={handleAddOrUpdateToDo}>
-          <Text style={{ color: '#FFF' }}>{isUpdating ? "Update" : "Add"}</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* FlatList to render the list of ToDo items */}
-      <FlatList
-        style={styles.list}
-        data={toDo}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => (
-          <ToDo
-            text={item.text}
-            updateMode={() => updateMode(item._id, item.text)}
-            deleteToDo={() => deleteToDo(item._id, setToDo)}
+        <View style={styles.top}>
+          {/* Input field to add new ToDo items */}
+          <TextInput
+            style={styles.input}
+            placeholder="Add ToDos..."
+            value={text}
+            onChangeText={value => setText(value)}
           />
-        )}
-      />
-    </View>
+
+          {/* Add or Update button based on the current mode */}
+          <TouchableOpacity style={styles.add} onPress={handleAddOrUpdateToDo}>
+            <Text style={{color: '#FFF'}}>{isUpdating ? 'Update' : 'Add'}</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* FlatList to render the list of ToDo items */}
+        <FlatList
+          style={styles.list}
+          data={toDo}
+          keyExtractor={item => item._id}
+          renderItem={({item}) => (
+            <ToDo
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo, Platform.OS)}
+            />
+          )}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
--- packages/mobile/api.js
+++ packages/mobile/api.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 // Set the base URL for the API
 // Use 'localhost' for development on the Android emulator
 // Change this to the appropriate URL when deploying the app
-const baseUrl = __DEV__ ? "http://10.0.2.2:1997" : "http://your-production-api-url";
+const baseUrl =  { android:"http://10.0.2.2:1997",ios:'http://localhost:1997' }
 
 // Function to get all ToDo items from the server
-const getAllToDo = (setToDo) => {
+const getAllToDo = (setToDo,platform) => {
   axios
-    .get(baseUrl)
+    .get(baseUrl[platform])
     .then(({ data }) => {
       console.log('Received data:', data);
       setToDo(data);
@@ -20,37 +20,37 @@ const getAllToDo = (setToDo) => {
 };
 
 /// Function to add a new ToDo item to the server
-const addToDo = (text, setText, setToDo) => {
+const addToDo = (text, setText, setToDo,platform) => {
     axios
-      .post(`${baseUrl}/save`, { text })
+      .post(`${baseUrl[platform]}/save`, { text })
       .then(() => {
         console.log('Successfully added Todo item.');
         setText(""); // Clear the input text after adding the Todo item
-        getAllToDo(setToDo); // Fetch all Todo items again to refresh the list
+        getAllToDo(setToDo,platform); // Fetch all Todo items again to refresh the list
       })
       .catch((err) => console.error('Error adding Todo item:', err));
   };
   
   // Function to update a ToDo item on the server
-  const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+  const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating,platform) => {
     axios
-      .post(`${baseUrl}/update`, { _id: toDoId, text })
+      .post(`${baseUrl[platform]}/update`, { _id: toDoId, text })
       .then(() => {
         console.log('Successfully updated Todo item.');
         setText(""); // Clear the input text after updating the Todo item
         setIsUpdating(false); // Exit update mode
-        getAllToDo(setToDo); // Fetch all Todo items again to refresh the list
+        getAllToDo(setToDo,platform); // Fetch all Todo items again to refresh the list
       })
       .catch((err) => console.error('Error updating Todo item:', err));
   };
   
   // Function to delete a ToDo item from the server
-  const deleteToDo = (_id, setToDo) => {
+  const deleteToDo = (_id, setToDo,platform) => {
     axios
-      .post(`${baseUrl}/delete`, { _id })
+      .post(`${baseUrl[platform]}/delete`, { _id })
       .then(() => {
         console.log('Successfully deleted Todo item.');
-        getAllToDo(setToDo); // Fetch all Todo items again to refresh the list
+        getAllToDo(setToDo,platform); // Fetch all Todo items again to refresh the list
       })
       .catch((err) => console.error('Error deleting Todo item:', err));
   };
